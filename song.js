27b8fd18a69c5ad47779b61c43208afab7d74899
GregSilvia <iamgregsilvia@gmail.com>
2023-10-21 03:03:31 -0700

    chrome extension final

--- song.js
+++ song.js
@@ -1,86 +1,149 @@
-document.addEventListener('DOMContentLoaded', function (){
-  const startRecognitionButton = document.getElementById('startButton');
+document.addEventListener('DOMContentLoaded', function () {
+  const startButton = document.getElementById('startButton');
   const recognizedText = document.getElementById('recognized-text');
+  const apiResults = document.getElementById('api-results');
+  const loadingSpinner = document.querySelector('.loading-spinner');
+  const circlesContainer = document.querySelector('.circles');
 
+
+
+  let isListening = false;
+  circlesContainer.style.display = 'none'
+  
+
+  // SPEECH CHECK
   if ('SpeechRecognition' in window || 'webkitSpeechRecognition' in window) {
     const recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition)();
 
+    // SPEECH RESULT
     recognition.onresult = function (event) {
-      const transcript  = event.results[0][0].transcript;
+      const transcript = event.results[0][0].transcript;
       recognizedText.textContent = transcript;
-    }
-    startRecognitionButton.addEventListener('click', function (){
-      recognizedText.textContent = ''
-      recognition.start();
+
+      // GET TEXT AND FETCH SONG
+      const speechString = transcript;
+      getTrackInfo(speechString);
+    };
+
+    // BUTTON LOGIC
+    startButton.addEventListener('click', function () {
+      if (isListening) {
+        recognition.stop();
+        isListening = false;
+        startButton.classList.remove('listening');
+        circlesContainer.style.display = 'block'
+      } else {
+        recognizedText.textContent = '';
+        recognition.start();
+        isListening = true;
+        startButton.classList.add('listening');
+        circlesContainer.style.display = 'block'
+      }
     });
+
+    // BUTTON END
+    recognition.onend = function () {
+      isListening = false;
+      startButton.classList.remove('listening');
+      circlesContainer.style.display = 'none'
+    };
   } else {
     recognizedText.textContent = "ERROR: Speech recognition not enabled on your browser";
   }
+  // API FETCH
+  async function getTrackInfo(string) {
+    const encodedString = encodeURIComponent(string);
+    const url = `https://spotify-scraper.p.rapidapi.com/v1/track/download?track=${encodedString}`;
+    const options = {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': 'abc01fe9edmsh081df58d5a2653bp1303e3jsn174d4f80cfd3',
+        'X-RapidAPI-Host': 'spotify-scraper.p.rapidapi.com'
+      }
+    };
+
+    try {
+      showLoadingSpinner()
+      const response = await fetch(url, options);
+
+      if (response.ok) {
+        const result = await response.json();
+        hideLoadingSpinner()
+
+        console.log(result);
+
+        // CLEAR PREVIOUS
+        apiResults.innerHTML = '';
+
+        if (result.status === true && result.spotifyTrack) {
+          const track = result.spotifyTrack;
+          const trackElement = createTrackElement(track);
+        
+          console.log(result);
+        
+
+
+          // ADD TRACK TO CONTAINER
+
+          apiResults.appendChild(trackElement);
+        } else {
+          apiResults.textContent = 'No tracks found.';
+        }
+      } else {
+        console.error(`Error: ${response.status} - ${response.statusText}`);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+   // CREATE ARTIST NAME, TRACK NAME, SPOT LINK
+   function createTrackElement(track) {
+    const trackElement = document.createElement('div');
+    trackElement.className = 'track-item';
+
+    const artistName = createTrackInfoElement(track.artists[0].name);
+    const trackName = createTrackInfoElement(track.name);
+
+    const spotifyLink = document.createElement('a');
+    spotifyLink.textContent = 'Spotify';
+    spotifyLink.href = track.shareUrl;
+    spotifyLink.target = '_blank';
+
+    if (track.album && track.album.cover && track.album.cover.length > 0) {
+      const albumArtwork = document.createElement('img');
+      albumArtwork.src = track.album.cover[0].url;
+      albumArtwork.alt = 'Album Artwork';
+      albumArtwork.classList.add('album-artwork');
+      trackElement.appendChild(albumArtwork);
+    }
+
+    // ADD ELEMENTS TO TRACK CONTAINER
+    trackElement.appendChild(artistName);
+    trackElement.appendChild(trackName);
+    trackElement.appendChild(spotifyLink);
+
+    return trackElement;
+  }
+
+  // HELPER FUNCTION
+  function createTrackInfoElement(text) {
+    const element = document.createElement('p');
+    element.textContent = text;
+    return element;
+  }
+// SHOW LOADING ANIMATION
+  function showLoadingSpinner() {
+    loadingSpinner.style.display = 'block';
+  }
+// HIDE LOADING ANIMATION
+  function hideLoadingSpinner() {
+    loadingSpinner.style.display = 'none';
+  }
+// CLEAR API RESULTS
+  function clearApiResults() {
+    while (apiResults.firstChild) {
+      apiResults.removeChild(apiResults.firstChild);
+    }
+  }
 });
-
-
-
-
-
-
-
-
-
-/*
-//speech to text api
-const SpeechRecognition =
-  window.SpeechRecognition || window.webkitSpeechRecognition;
-const SpeechGrammarList =
-  window.SpeechGrammarList || window.webkitSpeechGrammarList;
-const SpeechRecognitionEvent =
-  window.SpeechRecognitionEvent || window.webkitSpeechRecognitionEvent;
-
-  const recognition = new SpeechRecognition();
-  const speechRecognitionList = new SpeechGrammarList();
-
-  speechRecognitionList.addFromString(grammar, 1);
-
-  recognition.grammars = speechRecognitionList;
-recognition.continuous = false;
-recognition.lang = "en-US";
-recognition.interimResults = false;
-recognition.maxAlternatives = 1;
-
-const diagnostic = document.querySelector(".output");
-const bg = document.querySelector("html");
-const hints = document.querySelector(".hints");
-
-let colorHTML = "";
-colors.forEach((color, i) => {
-  console.log(color, i);
-  colorHTML += `<span style="background-color:${color};"> ${color} </span>`;
-});
-hints.innerHTML = `Tap or click then say a color to change the background color of the app. Try ${colorHTML}.`;
-
-document.body.onclick = () => {
-  recognition.start();
-  console.log("Ready to receive a color command.");
-};
-
-recognition.onresult = (event) => {
-    const color = event.results[0][0].transcript;
-    diagnostic.textContent = `Result received: ${color}.`;
-    bg.style.backgroundColor = color;
-    console.log(`Confidence: ${event.results[0][0].confidence}`);
-  };
-
-  recognition.onspeechend = () => {
-    recognition.stop();
-  };
-
-  recognition.onnomatch = (event) => {
-    diagnostic.textContent = "I didn't recognize that color.";
-  };
-
-  recognition.onerror = (event) => {
-    diagnostic.textContent = `Error occurred in recognition: ${event.error}`;
-  };
-
-  
-//then text to search
-*/
